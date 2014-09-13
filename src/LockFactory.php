85ce4d225b0f71aabc3e532758777a2918537fb0
Mathieu Rochette <mathieu@rochette.cc>
2014-09-13 11:32:05 +0200

    Fix issue found by Scrutinizer

--- src/LockFactory.php
+++ src/LockFactory.php
@@ -6,6 +6,17 @@ use Psr\Log\LoggerInterface;
 
 interface LockFactory
 {
+    /**
+     * Create a lock handle on $resource
+     * @param  string      $resource resource identifier
+     * @param  string|null $owner    owner name (for logging)
+     * @return Lock
+     */
     public function create($resource, $owner = null);
+
+    /**
+     * @param LoggerInterface $logger
+     * @return void
+     */
     public function setLogger(LoggerInterface $logger);
 }
